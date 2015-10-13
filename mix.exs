3ea95e55e35dbc1864124d60e71a62106dbe1e17
Michael Jochimsen <mlj@troglodytics.org>
2015-10-13 12:35:05 -0700

    Bump the version number, and remove the dialyzer mix task.
    The version number gets bumped to 0.1.0 (first test rev) and the dialyzer mix
    task gets pulled. While the task is useful, there is no need to keep it in the
    project as it will pollute the task space of projects which use the
    mock_server app.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MockServer.Mixfile do
   def project do
     [
       app: :mock_server,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -23,26 +23,6 @@ defmodule MockServer.Mixfile do
   # Application dependencies. See `mix help deps` for examples and options
   defp deps do
     []
-  end
-
-end
-
-defmodule Mix.Tasks.Dialyzer do
-
-  use Mix.Task
-
-  @shortdoc "Run the dialyzer"
-  @recursive true
-
-  @moduledoc """
-  Run the dialyzer over the generated POP3 application code.
-  """
-
-  @spec run(OptionParser.argv) :: :ok
-  def run(_) do
-    Mix.Task.run("compile")
-    ebin_path = Path.join([Mix.Project.app_path, "ebin"])
-    Mix.shell.cmd "dialyzer #{ebin_path}"
   end
 
 end
