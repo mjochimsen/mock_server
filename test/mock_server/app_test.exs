34562fbeb2dce00451a1e69b62d5e699a3bd42d0
Michael Jochimsen <mlj@troglodytics.org>
2015-10-14 16:00:06 -0700

    Fix a bug in the tests.
    A bug existed in the tests which would leave the mock_server application
    stopped after the AppTest module. This would cause any later tests which
    expected it to be running to fail (most notably the MockServerTest). Make sure
    the application is running when this test module is entered and exited by
    using the setup_all/2 macro and on_exit/1 function.

--- test/mock_server/app_test.exs
+++ test/mock_server/app_test.exs
@@ -1,10 +1,21 @@
 defmodule AppTest do
-  use ExUnit.Case
+
+  use ExUnit.Case, async: false
+
+  setup_all do
+    :ok = Application.ensure_started(:mock_server)
+    # Make sure the application is running when we're done
+    on_exit(fn -> Application.ensure_started(:mock_server) end)
+    :ok
+  end
 
   test "starting and stopping the mock server application" do
-    assert :ok = Application.ensure_started(:mock_server)
+    # Stop and restart the application
     assert :ok = Application.stop(:mock_server)
     assert :ok = Application.start(:mock_server)
+
+    # Again
     assert :ok = Application.stop(:mock_server)
+    assert :ok = Application.start(:mock_server)
   end
 end
